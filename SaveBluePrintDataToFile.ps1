aa45009f886a9436f712a61c518bac5ec4d61e14
Chris Mills <41859364+tridionation@users.noreply.github.com>
2018-09-10 20:07:25 +0100

    Post TDS update

--- SaveBluePrintDataToFile.ps1
+++ SaveBluePrintDataToFile.ps1
@@ -7,13 +7,17 @@ Save Blueprint data to disk
  
 using namespace Tridion.ContentManager.CoreService.Client
 
+# For Demo or Testing 
+# set to true to restrict to first 4 publications and 
+# slim down json to speed up saving (REDUCED DATA set)
+$DemoOrDebug = $false 
+
 $Modules = @(
 "Tridion-CoreService"
 )
-
 foreach($Module in $Modules)
 {
-    if(-not (Get-Module -Name $Module)){ Install-Module $Module  -Force }
+    if(-not (Get-Module -ListAvailable -Name $Module)){ Install-Module $Module  -Force }
     Import-Module $Module
 }
 
@@ -23,7 +27,7 @@ if(-not $credential){
 }
 
 $tcsconnection = @{
-        hostname       = "cms.poc2" 
+        hostname       = "cms.poc2.ucles.internal" 
         version        = "Web-8.5"   # 2011-SP1, 2013, 2013-SP1, Web-8.1, Web-8.5
         ConnectionType = "netTcp"    # Default, SSL, LDAP, LDAP-SSL, netTcp, Basic, Basic-SSL
         CredentialType = "Windows"
@@ -32,45 +36,70 @@ Set-TridionCoreServiceSettings @tcsconnection
 
 try
 {
+    Write-Progress -Activity "Tridion Core Service" -Status "Connecting ..."
     $client = Get-TridionCoreServiceClient
     $client.ChannelFactory.Credentials.Windows.ClientCredential = $credential
-    $client.GetApiVersion()
+    $client.GetApiVersion() | Out-Null
+    Write-Progress -Activity "Tridion Core Service" -Status "Connected to $($tcsconnection.hostname)"
+}
+catch 
+{
+    Write-Error "Failed to get data with TridionCoreService on $($tcsconnection.hostname)"
+    break
+}
 
 # Get Publications 
     $filter = New-Object PublicationsFilterData      
-    $SystemWideItems = $client.GetSystemWideList($Filter)
+    $publications = $client.GetSystemWideList($Filter)
 
 # Get Publications Data
-    $SystemWideItemsArray = @()
+    $DataArray = @()
     $counter = 0
     $total = $SystemWideItems.count
 
-    foreach ($SystemWideItem in $SystemWideItems)
+    if($DemoOrDebug){
+        $publications = $publications | Select-Object -First 4
+    }
+
+    foreach ($publication in $publications)
     {
         $counter += 1
         $progress = @{
-			Activity = "Get Publication $($SystemWideItem.Title)"
+			Activity = "Tridion Publication $($SystemWideItem.Title)"
 			PercentComplete = ($counter / $total) * 100
 			Status = "Processing $counter of $total" 
 		}
         Write-Progress @progress
-        $SystemWideItemsArray += $client.Read($SystemWideItem.Id,$null)
+
+        $hostname = $tcsconnection.hostname
+        $EncodedId = [System.Web.HttpUtility]::UrlEncode($publication.Id)      
+        $link = [string]::Format("http://{0}/SDL/#app=wcm&entry=cme&url=%23locationId%3D{1}",$hostname,$EncodedId )
+        $ts = Get-Date -Format u 
+
+        $ht = $client.Read($publication.Id,$null)
+        $ht | Add-Member -MemberType NoteProperty -Name "Date" -Value $ts
+        $ht | Add-Member -MemberType NoteProperty -Name "Link" -Value $link
+        
+        if($DemoOrDebug){
+            $ht.CategoriesXsd = $null
+            $ht.AccessControlList = $null
+        }
+        
+        $DataArray += $ht
     }
 
-    Write-Progress -Activity "Saving Data to file"
 
-    $client.Dispose()
+ 
+
 
 # Save Publication Data to file
-    $jfile = Join-path $PSScriptRoot -ChildPath "TridionBlueprint.json"
-    $SystemWideItemsArray | ConvertTo-Json -Depth 9 | Out-File -FilePath $jfile
-    Write-Progress -Completed -Activity "Saving Data to file"
-}
 
-catch 
-{
-    Write-Output "Failed to get data with TridionCoreService on $($tcsconnection.hostname)"
-    exit
-}
+$jfile = Join-path $PSScriptRoot -ChildPath "TridionBlueprint.json"
+Write-Progress -Activity "Tridion Data " -Status "Saving data to $jfile" 
+$DataArray | ConvertTo-Json -Depth 9 | Out-File -FilePath $jfile
+Write-Progress -Completed -Activity "Tridion Data" -Status "Saved data to $jfile" 
+
+
+
 
 
